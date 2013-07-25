ede87ff27372d72f3830e035dda91321790a3bfe
mbal <matteo.bana@gmail.com>
2013-07-25 11:13:48 +0200

    another trial at lexer

---
+++ src/basic.scm
@@ -0,0 +1,20 @@
+(module basic-lexer (trim string-literal dec-number char-literal identifier)
+                          ;line-comment block-comment)
+  (import chicken r5rs)
+  (import lexer parser utils)
+
+  (define (@ l x)
+    (let loop ((xs l))
+      (cond ((null? xs) (syntax-error "not found" x))
+            ((equal? (caar xs) x) (cadar xs))
+            (else (loop (cdr xs))))))
+
+  (define basic (make-lexer #t "" "" "" '(#\\ #\") anychar anychar))
+
+  (define trim (@ basic 'trim))
+  (define string-literal (@ basic 'string-literal))
+  (define dec-number (@ basic 'dec-number))
+  (define char-literal (@ basic 'char-literal))
+  (define identifier (@ basic 'identifier)))
+  ;(define line-comment (@ basic 'line-comment))
+  ;(define block-comment (@ basic 'block-comment)))
--- src/lexer.scm
+++ src/lexer.scm
@@ -1,54 +1,73 @@
-(module lexer (make-parser deflexer)
+(module lexer (export-definitions make-lexer)
   (import chicken r5rs)
   (import parser utils)
 
+  (define escape-error "invalid escape sequence")
+  (define white-space (char #\space))
   (define new-line (char #\newline))
 
-  (define escape-error "invalid escape sequence")
 
-  (define-syntax deflexer
+  ;; defines a closure and returns a list of all the definitions in the
+  ;; closure, in a list of 'name function pairs.
+  (define-syntax export-definitions
     (syntax-rules ()
-      ((deflexer lex (def name body) ...)
-       (define lex 
-         (begin 
+      ((export-definitions lex (def name body) ...)
+       (define lex
+         (begin
            (def name body) ...
            (list `(name ,name) ...))))))
 
-  (deflexer (make-parser 
-              comment-line-start
-              comment-block-start
-              comment-block-end
-              escapable-characters
-              identifier-start
-              identifier-letter)
+  (define (make-lexer skip-ws comment-line-start
+                      comment-block-start comment-block-end
+                      escapable-characters identifier-start
+                      identifier-letter) 
 
-    (defparser block-comment
-               (skip (between (str comment-block-start) 
-                              (str comment-block-end) 
-                              anychar)))
+    (begin
+      (defparser block-comment 
+                 (between (str comment-block-start) 
+                          (str comment-block-end) 
+                          anychar))
 
-    (defparser line-comment 
-               (skip
-                 (str comment-line-start)
-                 (many-until anychar (either new-line eof))))
+      (defparser line-comment 
+                 (then (str comment-line-start)
+                       (many-until anychar (either new-line eof))))
 
-    (define escape (one-of escapable-characters))
+      (define escape (one-of escapable-characters))
 
-    (defparser char-literal
-               (either
-                 (<?> (then (char #\\) escape) escape-error)
-                 (satisfy (lambda (x) (and (not (char=? x #\"))
-                                           (not (char=? x #\\)))))))
-    (defparser identifier 
-               (stringify 
-                 (then identifier-start (many identifier-letter))))
+      (define trim
+        (if skip-ws
+          (skip-many (either block-comment line-comment white-space))
+          (skip-many (either block-comment line-comment))))
 
-    (defparser dec-number
-               (named-bind
-                 (s <- (opt (one-of '(#\+ #\-)) #\+))
-                 (n <- (many1 digit))
-                 (succeed (list->number (cons s n)))))
+      (defparser char-literal
+                 (either
+                   (<?> (then (char #\\) escape) escape-error)
+                   (satisfy (lambda (x) (and (not (char=? x #\"))
+                                             (not (char=? x #\\)))))))
+      (defparser identifier 
+                 (after
+                   (stringify (then identifier-start (many identifier-letter)))
+                   trim))
 
-    (defparser string-literal
-               (stringify (between (char #\") (char #\") char-literal)))
-    ))
+      (defparser dec-number
+                 (after
+                   (named-bind
+                     (s <- (opt (one-of '(#\+ #\-)) #\+))
+                     (n <- (many1 digit))
+                     (succeed (list->number (cons s n))))
+                   trim))
+
+      (defparser string-literal
+                 (after
+                   (stringify (between (char #\") (char #\") char-literal))
+                   trim))
+
+      (list `(string-literal ,string-literal)
+            `(dec-number ,dec-number)
+            `(identifier ,identifier)
+            `(char-literal ,char-literal)
+            `(trim ,trim)
+            `(line-comment ,line-comment)
+            `(block-comment ,block-comment))
+      ))
+    )
