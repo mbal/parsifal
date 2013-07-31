827d73d7b6e3af5fe19250dfd15e32df7d9a63d1
mbal <matteo.bana@gmail.com>
2013-07-31 16:10:49 +0200

    forgot to rewrite symbolic parsers

--- src/operator.scm
+++ src/operator.scm
@@ -31,14 +31,20 @@
   ;; Same as the parsers above, but, instead of applying the operation, they
   ;; return a list of (op a b), useful to create a parse tree.
   ;;
-  (create-op-parser add-op-s (one-of '(#\+ #\-))
-                    #\+ => (lambda (a b) (list '+ a b))
-                    #\- => (lambda (a b) (list '- a b)))
 
-  (create-op-parser mul-op-s (one-of '(#\* #\/))
-                    #\* => (lambda (a b) (list '* a b)) 
-                    #\/ => (lambda (a b) (list '/ a b)))
+  (define add-op-s
+    (named-bind
+      (o <- (one-of '(#\+ #\-)))
+      (succeed (if (equal? #\+ o)
+                 (lambda (a b) (list '+ a b))
+                 (lambda (a b) (list '- a b))))))
 
+  (define mul-op-s
+    (named-bind
+      (o <- (one-of '(#\* #\/ #\%)))
+      (succeed (cond ((equal? o #\*) (lambda (a b) (list '* a b)))
+                     ((equal? o #\/) (lambda (a b) (list '/ a b)))
+                     (else (lambda (a b) (list 'remainder a b)))))))
   (define exp-op-s
     (then (char #\^)
           (succeed (lambda (a b) (list 'expt a b)))))
