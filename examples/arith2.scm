f95ac297bb6bf4e7179498a57eabfd93a30a774b
mbal <matteo.bana@gmail.com>
2013-07-31 15:46:32 +0200

    updated example

--- examples/arith2.scm
+++ examples/arith2.scm
@@ -4,19 +4,23 @@
 
   ;;; this example is equivalent to the example in `arith.scm`, however, it's
   ;;; implemented from a higher level point of view, since we use combinators
-  ;;; and we don't write the primitives ourselves.
+  ;;; and we don't write the primitives ourselves. The other difference is 
+  ;;; that here we interpret the operation during parsing.
 
   (defparser s
      (either
        (named-bind 
          (char #\() 
-         (e <- expr2)
+         (e <- expr)
          (char #\))
          (succeed e))
        number))
 
-  (define f (chainl1 s2 mul-op))
-  (define expr (chainl1 f2 add-op))
+  ;; We adopt the (usual) convention that power is associative to the right.
+  (define exponentiation (chainr1 s exp-op))
+
+  (define factor (chainl1 exponentiation mul-op))
+  (define expr (chainl1 factor add-op))
   
   (define (run-arith e)
     (parse expr e))
